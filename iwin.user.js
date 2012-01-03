76c8bf0620e396c47596eb2a04126c8029e67dfb
Zac <mymengyu@gmail.com>
2012-01-03 16:51:21 +0800

    Intial Commit

---
+++ iwin.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name            iWin
+// @namespace       iWin
+// @description     修正官方網站不想解決的問題
+// @include         http://www.i-win.com.tw/*
+// @version         0.1
+// ==/UserScript==
+
+document.getElementById("middlearea").height = "800";
